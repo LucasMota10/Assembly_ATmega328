c566a283036c5cc9abb2039f94d1518dee6b358b
Lucas Barbosa Bulcão Mota <101370558+LucasMota10@users.noreply.github.com>
2025-06-03 18:31:15 -0300

    att multiplex
    consertar delay, logica correta

--- Display_7_Segmentos/Multiplexador_2segmentos/main.asm
+++ Display_7_Segmentos/Multiplexador_2segmentos/main.asm
@@ -16,39 +16,6 @@
 .def aux = r16
 .def Decimal = r17
 
-.org 0x0000
-	rjmp main
-
-zero_decimal:
-	cbi PORTB, Control_B1; B1 = 0
-	sbi PORTB, Control_B2; B2 = 1
-	ldi aux, 0b00111111
-	out PORTD, aux
-	inc Decimal
-	rcall delay_500ms
-	rjmp loop
-
-default:
-	ldi REG, 0x00
-	rjmp loop
-
-zero:
-	cbi PORTB, Control_B1; B1 = 1
-	sbi PORTB, Control_B2; B2 = 0
-	ldi aux, 0b00111111
-	out PORTD, aux
-	inc REG
-	rcall delay_500ms
-	rjmp loop 
-um:
-	cbi PORTB, Control_B1; B1 = 1
-	sbi PORTB, Control_B2; B2 = 0
-	ldi aux, 0b00000110
-	out PORTD, aux
-	inc REG
-	rcall delay_500ms
-	rjmp loop
-	
 main:
 	sbi DDRD, LED_PIN0
 	sbi DDRD, LED_PIN1
@@ -66,97 +33,26 @@ main:
 	
 	cbi PORTB, Control_B1; Bit 0 em B1
 	cbi PORTB, Control_B2; Bit 0 em B2
+
 loop:
+
+	rcall show_dec
+	rcall delay_multiplex
+	rcall show_uni
+	rcall delay_multiplex
+
+	rcall delay_contagem
+	inc REG
+	rjmp loop
+
+show_dec:
+	
+	cpi Decimal, 6
+	BRSH default_decimal
+
 	cpi Decimal, 0
 	BREQ zero_decimal
-	
-	cpi REG, 10
-	BRSH default
-	cpi REG, 0
-	BREQ zero
-	cpi REG, 1
-	BREQ um
-	cpi REG, 2
-	BREQ dois
-	cpi REG, 3
-	BREQ tres
-	cpi REG, 4
-	BREQ quatro
-	cpi REG, 5
-	BREQ cinco
-	cpi REG, 6
-	BREQ seis
-	cpi REG, 7
-	BREQ sete
-	cpi REG,8
-	BREQ oito
-	cpi REG, 9
-	BREQ nove
 
-
-dois:
-	cbi PORTB, Control_B1; B1 = 1
-	sbi PORTB, Control_B2; B2 = 0
-	ldi aux, 0b01011011
-	out PORTD, aux
-	inc REG
-	rcall delay_500ms
-	rjmp loop
-tres:
-	cbi PORTB, Control_B1; B1 = 1
-	sbi PORTB, Control_B2; B2 = 0
-	ldi aux, 0b01001111
-	out PORTD, aux
-	inc REG
-	rcall delay_500ms
-	rjmp loop
-quatro:
-	cbi PORTB, Control_B1; B1 = 1
-	sbi PORTB, Control_B2; B2 = 0
-	ldi aux, 0b01100110
-	out PORTD, aux
-	inc REG
-	rcall delay_500ms
-	rjmp loop
-cinco:
-	cbi PORTB, Control_B1; B1 = 1
-	sbi PORTB, Control_B2; B2 = 0
-	ldi aux, 0b01101101
-	out PORTD, aux
-	inc REG
-	rcall delay_500ms
-	rjmp loop
-seis:
-	cbi PORTB, Control_B1; B1 = 1
-	sbi PORTB, Control_B2; B2 = 0
-	ldi aux, 0b01111101
-	out PORTD, aux
-	inc REG
-	rcall delay_500ms
-	rjmp loop
-sete:
-	cbi PORTB, Control_B1; B1 = 1
-	sbi PORTB, Control_B2; B2 = 0
-	ldi aux, 0b00000111
-	out PORTD, aux
-	inc REG
-	rcall delay_500ms
-	rjmp loop 
-oito:
-	cbi PORTB, Control_B1; B1 = 1
-	sbi PORTB, Control_B2; B2 = 0
-	ldi aux, 0b01111111
-	out PORTD, aux
-	inc REG
-	rcall delay_500ms
-	rjmp loop 
-nove:
-	cbi PORTB, Control_B1; B1 = 1
-	sbi PORTB, Control_B2; B2 = 0
-	ldi aux, 0b01101111
-	out PORTD, aux
-	inc REG
-	
 	cpi Decimal, 1
 	BREQ um_decimal
 	
@@ -171,72 +67,215 @@ nove:
 	
 	cpi Decimal, 5
 	BREQ cinco_decimal
+
+	ret
+
+default_decimal:
 	
-	cpi Decimal, 6
-	BRSH default_decimal
-	
-	
-	rcall delay_500ms
-	rjmp loop 
-	
+	ldi Decimal, 0x00
+
+	ret
+
+zero_decimal:
+
+	cbi PORTB, Control_B1; B1 = 0
+	sbi PORTB, Control_B2; B2 = 1
+	ldi aux, 0b00111111
+	out PORTD, aux
+
+	ret
 um_decimal:
+	
 	cbi PORTB, Control_B1; B1 = 0
 	sbi PORTB, Control_B2; B2 = 1
 	ldi aux, 0b00000110
 	out PORTD, aux
-	inc Decimal
-	rcall delay_500ms
-	rjmp loop
+	ret
+
 dois_decimal:
+	
 	cbi PORTB, Control_B1; B1 = 0
 	sbi PORTB, Control_B2; B2 = 1
 	ldi aux, 0b01011011
 	out PORTD, aux
-	inc Decimal
-	rcall delay_500ms
-	rjmp loop
+	
+
+	ret
+
 tres_decimal:
+	
 	cbi PORTB, Control_B1; B1 = 0
 	sbi PORTB, Control_B2; B2 = 1
 	ldi aux, 0b01001111
 	out PORTD, aux
-	inc Decimal
-	rcall delay_500ms
-	rjmp loop
+	
+	ret
+
 quatro_decimal:
+
 	cbi PORTB, Control_B1; B1 = 0
 	sbi PORTB, Control_B2; B2 = 1
 	ldi aux, 0b01100110
 	out PORTD, aux
-	inc Decimal
-	rcall delay_500ms
-	rjmp loop
+	
+	ret
+
 cinco_decimal:
+	
 	cbi PORTB, Control_B1; B1 = 0
 	sbi PORTB, Control_B2; B2 = 1
 	ldi aux, 0b01101101
 	out PORTD, aux
-	inc Decimal
-	rcall delay_500ms
-	rjmp loop	
-
-default_decimal:
-	ldi Decimal, 0x00
-	rjmp loop
-
-delay_500ms:
-	ldi r20,132 
-delay2:
-	ldi r19,164
-delay1:
-	ldi r18,128
-delay0:	
-	dec r18
-	brne delay0
-	dec r19
-	brne delay1
-	dec r20
-	brne delay2
+	
 	ret
 
+show_uni:
 	
+	cpi REG, 10
+	BRSH default_uni
+
+	cpi REG, 0
+	BREQ zero
+
+	cpi REG, 1
+	BREQ um
+
+	cpi REG, 2
+	BREQ dois
+
+	cpi REG, 3
+	BREQ tres
+
+	cpi REG, 4
+	BREQ quatro
+
+	cpi REG, 5
+	BREQ cinco
+
+	cpi REG, 6
+	BREQ seis
+
+	cpi REG, 7
+	BREQ sete
+
+	cpi REG,8
+	BREQ oito
+
+	cpi REG, 9
+	BREQ nove
+
+	ret
+default_uni:
+
+	ldi REG, 0x00
+	ret
+zero:
+	sbi PORTB, Control_B1; B1 = 1
+	cbi PORTB, Control_B2; B2 = 0
+	ldi aux, 0b00111111
+	out PORTD, aux
+	
+	ret
+
+um:
+
+	sbi PORTB, Control_B1; B1 = 1
+	cbi PORTB, Control_B2; B2 = 0
+	ldi aux, 0b00000110
+	out PORTD, aux
+	
+	ret
+
+dois:
+
+	sbi PORTB, Control_B1; B1 = 1
+	cbi PORTB, Control_B2; B2 = 0
+	ldi aux, 0b01011011
+	out PORTD, aux
+	
+	ret
+tres:
+	
+	sbi PORTB, Control_B1; B1 = 1
+	cbi PORTB, Control_B2; B2 = 0
+	ldi aux, 0b01001111
+	out PORTD, aux
+	
+	ret
+quatro:
+	
+	sbi PORTB, Control_B1; B1 = 1
+	cbi PORTB, Control_B2; B2 = 0
+	ldi aux, 0b01100110
+	out PORTD, aux
+	
+
+	ret
+cinco:
+
+	sbi PORTB, Control_B1; B1 = 1
+	cbi PORTB, Control_B2; B2 = 0
+	ldi aux, 0b01101101
+	out PORTD, aux
+	
+
+	ret
+
+seis:
+	
+	sbi PORTB, Control_B1; B1 = 1
+	cbi PORTB, Control_B2; B2 = 0
+	ldi aux, 0b01111101
+	out PORTD, aux
+
+	ret
+sete:
+	
+	sbi PORTB, Control_B1; B1 = 1
+	cbi PORTB, Control_B2; B2 = 0
+	ldi aux, 0b00000111
+	out PORTD, aux
+	
+	
+	ret
+oito:
+	
+	sbi PORTB, Control_B1; B1 = 1
+	cbi PORTB, Control_B2; B2 = 0
+	ldi aux, 0b01111111
+	out PORTD, aux
+	
+	ret
+nove:
+	
+	sbi PORTB, Control_B1; B1 = 1
+	cbi PORTB, Control_B2; B2 = 0
+	ldi aux, 0b01101111
+	out PORTD, aux
+	inc Decimal
+	ret
+
+
+delay_multiplex:
+	ldi r18, 2
+dmp_outer:
+	ldi r19, 100
+dmp_inner:
+	nop
+	dec r19
+	brne dmp_inner
+	dec r18
+	brne dmp_outer
+	ret
+
+delay_contagem:
+	ldi r18, 255
+dc_outer:
+	ldi r19, 255
+dc_inner:
+	nop
+	dec r19
+	brne dc_inner
+	dec r18
+	brne dc_outer
+	ret
